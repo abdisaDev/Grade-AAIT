7554f14b9cbb6a2ab88b5690300d3661620eafe5
Abdisa Dev <abdisadev@gmail.com>
2025-04-20 17:37:05 +0300

    dsadsa

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { checkGrade } from "./scrape";
+import { checkGrade } from "./scrape.js";
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.get("/", async (_req, res) => {
-    res.send("Hello from EMS :)");
+  res.send("Hello from EMS :)");
 });
 app.post("/grades", async (_req, res) => {
-    const payload = _req.body;
-    const grades = await checkGrade(payload);
-    res.send(grades);
+  const payload = _req.body;
+  const grades = await checkGrade(payload);
+  res.send(grades);
 });
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
