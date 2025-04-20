8f8a3cec23f8a4be71c4c73368f45d68d8e403eb
Abdisa Dev <abdisadev@gmail.com>
2025-04-20 18:43:07 +0300

    dsadsa

--- dist/scrape.js
+++ dist/scrape.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 export async function checkGrade(payload) {
   const browser = await puppeteer.launch({
+    headless: "new",
     args: [
       "--no-sandbox",
       "--single-process",
